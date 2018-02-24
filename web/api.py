43675dc2295a527f402d73a0faab54615b1ac145
raryosu <me@raryosu.info>
2018-02-24 17:12:29 +0900

    ざっとAPIつくった

---
+++ web/api.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import json
+from flask import Flask, jsonify, abort
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'hello:)'
+
+
+@app.route('/api/', methods=['GET'])
+def get():
+    return jsonify(read_model())
+
+
+@app.route('/api/<school_id>/', methods=['GET'])
+def get_school_info(school_id):
+    # TODO: KeyError の Handling
+    school_info = get_model()[school_id]
+    return jsonify(school_info)
+
+
+@app.route('/404')
+def abort404():
+    abort(404)
+
+
+def get_model():
+    """ Get model """
+    return read_model()
+
+
+def read_model():
+    """ Load JSON """
+    file_name = "../data/data.json"
+    try:
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    except IOError as e:
+        print(e)
+        return None
+
+
+if __name__ == '__main__':
+    app.run()
